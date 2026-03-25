c8cafdabb584a6d7e52ab2fe0ca6182609d6c54a
Ayushi <ayushi.rajak11@gmail.com>
2026-03-25 22:50:59 +0530

    JS fixes and formatting

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/**
+ * @type {import("prettier").Config}
+ */
+export default {
+    tabWidth: 4,
+    useTabs: true,
+    trailingComma: "all",
+    singleQuote: false,
+    bracketSpacing: true,
+    arrowParens: "avoid",
+    printWidth: 100,
+    bracketSameLine: false,
+    semi: true,
+    quoteProps: "consistent",
+};
--- script.js
+++ script.js
@@ -1,53 +1,67 @@
-function registerUser(){
-    let name = document.getElementById("name").value;
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+console.log("js working");
 
-    localStorage.setItem("username", username);
-    localStorage.setItem("password", password);
-    
-    alert("Account Created");
-    window.location.href = "login.html";
+// Registration page
+function registerUser() {
+  let name = document.getElementById("name").value;
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  localStorage.setItem("username", username);
+  localStorage.setItem("password", password);
+
+  alert("Account Created");
+  window.location.href = "login.html";
 }
+// Login page
+function loginUser(event) {
+  event.preventDefault();
 
-function loginUser(event){
-    event.preventDefault();
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
 
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+  let savedUser = localStorage.getItem("username");
+  let savedPass = localStorage.getItem("password");
 
-    let savedUser = localStorage.getItem("username");
-    let savedPass = localStorage.getItem("password");
-
-    if(username === savedUser && password === savedPass){
-
+  if (username === savedUser && password === savedPass) {
     alert("Login successful!");
     window.location.href = "Create/journal.html";
-    }
-    else{
+  } else {
     alert("incorrect email or password");
-    }
-    }
+  }
+}
+// Journal
+function saveEntry(event) {
+  event.preventDefault();
 
-function saveEntry(){
-    event.preventDefault();
+  let title = document.getElementById("main-title").value;
+  let category = document.getElementById("journal-category").value;
+  let entry = document.getElementById("journal-entry").value;
 
-    let title = document.getElementById("main-title").value;
-    let category = document.getElementById("journal-category").value;
-    let entry = document.getElementById("journal-entry").value;
+  let journals = JSON.parse(localStorage.getItem("journals")) || [];
 
-    let journals = JSON.parse(localStorage.getItem("journals")) || [];
+  journals.push({
+    title: title,
+    category: category,
+    entry: entry,
+  });
 
-    journals.push({
-        title:title,
-        category:category,
-        entry:entry
-        
-    });
+  localStorage.setItem("journals", JSON.stringify(journals));
+  alert("Entry Saved");
 
-    localStorage.setItem("journals", JSON.stringify(journals));
-    displayEntries();
+  document.querySelector(".journal-form").reset();
+  displayEntries();
+}
 
-    document.querySelector("journal-form").reset();
+function logoutUser(){
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+    alert("Logout successful!");
+    window.location.href="login.html";
+}
 
+// Scrapbook page
+function previewImage(event, id) {
+  let image = document.getElementById(id);
+
+  image.src = URL.createObjectURL(event.target.files[0]);
 }
