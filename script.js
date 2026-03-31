4420c56547f19c576611ad83b98d2cc5d9f5e320
Ayushi <ayushi.rajak11@gmail.com>
2026-03-31 19:18:48 +0530

    update

--- script.js
+++ script.js
@@ -2,66 +2,66 @@ console.log("js working");
 
 // Registration page
 function registerUser() {
-  let name = document.getElementById("name").value;
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
+	let name = document.getElementById("name").value;
+	let username = document.getElementById("username").value;
+	let password = document.getElementById("password").value;
 
-  localStorage.setItem("username", username);
-  localStorage.setItem("password", password);
+	localStorage.setItem("username", username);
+	localStorage.setItem("password", password);
 
-  alert("Account Created");
-  window.location.href = "login.html";
+	alert("Account Created");
+	window.location.href = "login.html";
 }
 // Login page
 function loginUser(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
+	let username = document.getElementById("username").value;
+	let password = document.getElementById("password").value;
 
-  let savedUser = localStorage.getItem("username");
-  let savedPass = localStorage.getItem("password");
+	let savedUser = localStorage.getItem("username");
+	let savedPass = localStorage.getItem("password");
 
-  if (username === savedUser && password === savedPass) {
-    alert("Login successful!");
-    window.location.href = "Create/journal.html";
-  } else {
-    alert("incorrect email or password");
-  }
+	if (username === savedUser && password === savedPass) {
+		alert("Login successful!");
+		window.location.href = "Create/journal.html";
+	} else {
+		alert("incorrect email or password");
+	}
 }
 // Journal
 function saveEntry(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  let title = document.getElementById("main-title").value;
-  let category = document.getElementById("journal-category").value;
-  let entry = document.getElementById("journal-entry").value;
+	let title = document.getElementById("main-title").value;
+	let category = document.getElementById("journal-category").value;
+	let entry = document.getElementById("journal-entry").value;
 
-  let journals = JSON.parse(localStorage.getItem("journals")) || [];
+	let journals = JSON.parse(localStorage.getItem("journals")) || [];
 
-  journals.push({
-    title: title,
-    category: category,
-    entry: entry,
-  });
+	journals.push({
+		title: title,
+		category: category,
+		entry: entry,
+	});
 
-  localStorage.setItem("journals", JSON.stringify(journals));
-  alert("Entry Saved");
+	localStorage.setItem("journals", JSON.stringify(journals));
+	alert("Entry Saved");
 
-  document.querySelector(".journal-form").reset();
-  displayEntries();
+	document.querySelector(".journal-form").reset();
+	displayEntries();
 }
 
 function logoutUser() {
-  localStorage.removeItem("username");
-  localStorage.removeItem("password");
-  alert("Logout successful!");
-  window.location.href = "/login.html";
+	localStorage.removeItem("username");
+	localStorage.removeItem("password");
+	alert("Logout successful!");
+	window.location.href = "/index.html";
 }
 
 // Scrapbook page
 function previewImage(event, id) {
-  let image = document.getElementById(id);
+	let image = document.getElementById(id);
 
-  image.src = URL.createObjectURL(event.target.files[0]);
+	image.src = URL.createObjectURL(event.target.files[0]);
 }
